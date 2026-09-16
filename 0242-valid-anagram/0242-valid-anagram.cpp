60ded9ace92e8d8f61181712f9d3754d60cb813d
OVERLORD <adityadeshmukh1992007@gmail.com>
2026-09-16 18:55:20 +0530

    Time: 0 ms (100%), Space: 9.7 MB (75.02%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(std::string s, std::string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int counts[26] = {0};
+
+        for (int i = 0; i < s.length(); ++i) {
+            counts[s[i] - 'a']++;
+            counts[t[i] - 'a']--;
+        }
+
+        for (int count : counts) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
